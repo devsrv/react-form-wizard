1be3c4922865affab6c95dc1a289c55a674bccb1
Sourav R <developer.srv1@gmail.com>
2022-10-09 13:13:23 +0530

    added tailwind build script

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './index.css' // remove from production environment
+import './tailwind.min.css' // remove from development environment
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
