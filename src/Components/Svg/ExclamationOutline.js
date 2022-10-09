a59aadba0e8c01726f911062c393668a23e91f76
rsrv1 <115305121+rsrv1@users.noreply.github.com>
2022-10-09 14:03:07 +0530

    missing react import fix

--- src/Components/Svg/ExclamationOutline.js
+++ src/Components/Svg/ExclamationOutline.js
@@ -1,16 +1,17 @@
+import React from 'react'
+
 export default () => (
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		className="h-5 w-5 mt-0.5 text-white mr-1"
-		fill="none"
-		viewBox="0 0 24 24"
-		stroke="currentColor"
-		strokeWidth={2}
-	>
-		<path
-			strokeLinecap="round"
-			strokeLinejoin="round"
-			d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-		/>
-	</svg>
-);
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5 mt-0.5 text-white mr-1"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}>
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+        />
+    </svg>
+)
